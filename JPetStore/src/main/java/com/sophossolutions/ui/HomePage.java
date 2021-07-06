dc9d0f6b247f6ab0b87bdbd872b020903eeb3fbb
juanBlancoSophos <juan.blanco@sophossolutions.com>
2021-07-06 14:48:15 -0500

    Traer register

--- JPetStore/src/main/java/com/sophossolutions/ui/HomePage.java
+++ JPetStore/src/main/java/com/sophossolutions/ui/HomePage.java
@@ -2,7 +2,8 @@ package com.sophossolutions.ui;
 
 import net.serenitybdd.screenplay.targets.Target;
 
-public class HomePage {
+public class HomePage 
+{
 	public static final Target LINK_TEXT_SIGN_IN = Target.the("Link for sign user").locatedBy("//a[contains(text(),'Sign In')]");
 	public static final Target TXT_WELCOME = Target.the("Text for user logged").locatedBy("//div[contains(text(),'Welcome')]");
 }
