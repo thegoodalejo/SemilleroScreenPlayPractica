5f8dbd00f6ab1bcabb9aaeee217cf5dc8384ab7d
Andres Rodrigo Lopez Realpe <andres.lopez@sophossolutions.com>
2021-07-06 15:10:40 -0500

    Actualizacion Login 2

--- TiendaMascotasSP/src/main/java/com/sophossolutions/ui/HomePage.java
+++ TiendaMascotasSP/src/main/java/com/sophossolutions/ui/HomePage.java
@@ -4,4 +4,7 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class HomePage {
 	public static final Target BTN_SIGNIN = Target.the("Boton iniciar secion").locatedBy("//a[text()='Sign In']");
+	public static final Target WELCOME_MESSAGE = Target.the("Boton iniciar secion").locatedBy("//div[@id='WelcomeContent']");
+	
+	
 }
