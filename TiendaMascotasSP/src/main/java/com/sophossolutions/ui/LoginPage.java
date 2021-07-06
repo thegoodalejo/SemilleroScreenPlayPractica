405240f632e4c5485200e4547ca92da54b69d3b6
Andres Ricardo  Guarnizo Ocampo <andres.guarnizo@sophossolutions.com>
2021-07-06 15:39:38 -0500

    modeloRegister

--- TiendaMascotasSP/src/main/java/com/sophossolutions/ui/LoginPage.java
+++ TiendaMascotasSP/src/main/java/com/sophossolutions/ui/LoginPage.java
@@ -9,5 +9,6 @@ public class LoginPage {
 	public static final Target TXT_USERNAME = Target.the("Campo para el usuario").located(By.name("username"));
 	public static final Target TXT_PASSWORD = Target.the("Campo para la contraseña").located(By.name("password"));
 	public static final Target BTN_LOGIN = Target.the("Boton Login").located(By.name("signon"));
+	public static final Target BTN_REGISTER_NOW = Target.the("Boton Boton registrar ahora").locatedBy("//a[text()='Register Now!']");
 
 }
