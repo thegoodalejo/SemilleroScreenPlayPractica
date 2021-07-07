4018c7ac31549f48f5ca00673c3a702c829e0047
DuvanCeballos <elber.ceballos@sophossolutions.com>
2021-07-07 14:50:47 -0500

    version 4.6

--- ProyectoTiendaMascotas/lib/src/main/java/com/sophossolutions/ui/StoreCar.java
+++ ProyectoTiendaMascotas/lib/src/main/java/com/sophossolutions/ui/StoreCar.java
@@ -4,4 +4,5 @@ import net.serenitybdd.screenplay.targets.Target;
 
 public class StoreCar {
 	public static final Target PET_SELECTED = Target.the("PET SELECT").locatedBy("//*[contains(text(), '{0}')]");
+	public static final Target BTN_REMOVE = Target.the("PET SELECT").locatedBy("//td[contains(text(), '{0}')]//ancestor::tr//a[@class]");
 }
