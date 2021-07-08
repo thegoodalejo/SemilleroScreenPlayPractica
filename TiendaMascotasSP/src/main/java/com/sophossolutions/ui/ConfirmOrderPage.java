bf7d07c14965286ca7db111b5176e778ee7dc16d
Andres Ricardo  Guarnizo Ocampo <andres.guarnizo@sophossolutions.com>
2021-07-07 19:18:26 -0500

    ActualizacionOrderPage

---
+++ TiendaMascotasSP/src/main/java/com/sophossolutions/ui/ConfirmOrderPage.java
@@ -0,0 +1,9 @@
+package com.sophossolutions.ui;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class ConfirmOrderPage {
+	
+	public static final Target BTN_CONFIRM = Target.the("Boton de confirmar datos").locatedBy("//a[text()='Confirm']");
+
+}
