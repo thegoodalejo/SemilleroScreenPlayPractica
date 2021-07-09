5c06baf91d932cd034d3ef99ab456cac09a3b4b3
Santiago Andres Espínal Mendoza <santiago.espinal@sophossolutions.com>
2021-07-08 23:54:54 -0500

    Service GET and Post in gorest

--- JPetStore/src/main/java/com/sophossolutions/util/Constants.java
+++ JPetStore/src/main/java/com/sophossolutions/util/Constants.java
@@ -8,6 +8,7 @@ public class Constants
 	public static final String ACTOR_NAME = "Juan Miguel y Santaigo";
 	public static final String REMEMBER = "NUMBER_ACOUNT";
 	public static final String MODEL_CARRITO = "ModeloCarrito";
+	public static final String TOKEN_GOREST = "837d26a1749dbe0896d8d274624a165ddee934c497733fd9430d9af57e9f2f10";
 	public static final String MODEL_ORDER = "ModelOrder";
 	public static final String DIR_INDEX_ANIMAL = "indexAnimal";
 	public static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
